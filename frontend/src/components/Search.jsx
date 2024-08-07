37b4cd9ee95f6895924c267e6291e8d0778ec4c7
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-08-07 15:47:27 +0100

    updated the search functionality

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,10 +1,26 @@
+import { useRef, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 
-const Search = () => {
+const Search = ({ ref }) => {
+  const [search, setSearch] = useState("");
+
   return (
     <div>
-      {" "}
-      <CiSearch size={30} color="white" />
+      <div className="flex w-full lg:w-[22rem] bg-slate-100 rounded-md">
+        <button className="bg-orange-600 w-20 flex items-center justify-center rounded-l">
+          <CiSearch size={30} color="white" />
+        </button>
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="search"
+          name="search"
+          ref={ref}
+          id="search"
+          className="w-[90%] py-3 px-2 rounded-md  outline-none"
+          placeholder="Search Pharmakart..."
+        />
+      </div>
     </div>
   );
 };
